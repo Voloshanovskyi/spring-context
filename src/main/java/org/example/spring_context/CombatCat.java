32c7e18cdd10dcfdfac33b23fd0094bd70efd7ac
Oleh <olvolo17@gmail.com>
2024-06-26 18:45:41 +0300

    Create @CalculateCatStrength

--- src/main/java/org/example/spring_context/CombatCat.java
+++ src/main/java/org/example/spring_context/CombatCat.java
@@ -2,30 +2,24 @@ package org.example.spring_context;
 
 public class CombatCat {
     private String name;
-    private Integer strength;
     @CalculateCatStrength(minStrength = 10, maxStrength = 15)
 
+    private Integer strength;
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("CombatCat{");
-        sb.append("name='").append(name).append('\'');
-        sb.append(", strength=").append(strength);
-        sb.append('}');
-        return sb.toString();
+        return "CombatCat{" + "name='" + name + '\'' +
+               ", strength=" + strength +
+               '}';
     }
-
     public Integer getStrength() {
         return strength;
     }
-
     public void setStrength(final Integer strength) {
         this.strength = strength;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(final String name) {
         this.name = name;
     }
--- src/main/java/org/example/spring_context/FightClubImpl.java
+++ src/main/java/org/example/spring_context/FightClubImpl.java
@@ -3,25 +3,20 @@ package org.example.spring_context;
 public class FightClubImpl implements FightClub {
     private CombatCat loriCat;
     private CombatCat baxterCat;
-
     public void setLoriCat(final CombatCat loriCat) {
         this.loriCat = loriCat;
     }
-
     public void setBaxterCat(final CombatCat baxterCat) {
         this.baxterCat = baxterCat;
     }
-
     @Override
     public void fight() {
         System.out.println("First cat: " + loriCat.toString());
         System.out.println("Second cat: " + baxterCat.toString());
-
         Integer loriCatStrength = loriCat.getStrength();
         Integer baxterCatStrength = baxterCat.getStrength();
         String loriCatName = loriCat.getName();
         String baxterCatName = baxterCat.getName();
-
         if (loriCatStrength > baxterCatStrength) {
             System.out.println(loriCatName + " cat win");
         } else if (baxterCatStrength > loriCatStrength) {
@@ -30,7 +25,5 @@ public class FightClubImpl implements FightClub {
             System.out.println("Cats strength is the same");
         }
     }
-
-
 }
 
